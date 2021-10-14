1b8f2cf51049d32db11d47668c2bc89ce21437a9
Alex Marriott <alex.marriott@buildcircle.co.uk>
2021-10-14 14:03:48 +0100

    init app

---
+++ index.js
@@ -0,0 +1,21 @@
+class Game {
+  constructor() {
+    const cards = [0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8];
+    const player1Cards = [...cards].sort(() => Math.random() - 0.5)
+    const player2Cards = [...cards].sort(() => Math.random() - 0.5)
+    this.player1 = {
+      health: 30, manaSlot: 0, activeMana: 0, cards: player1Cards, cardsInHand: player1Cards.splice(0, 3),
+    };
+    this.player2 = {
+      health: 30, manaSlot: 0, activeMana: 0, cards: player2Cards, cardsInHand: player2Cards.splice(0, 3),
+    };
+  }
+
+  turnStart() {
+    this.player1.manaSlot += 1
+    this.player1.activeMana = this.player1.manaSlot
+    this.player1.cardsInHand = [...this.player1.cardsInHand, this.player1.cards.splice(0, 1)]
+  }
+}
+
+module.exports = Game;
---
+++ index.test.js
@@ -0,0 +1,64 @@
+const Game = require("./index")
+
+describe("trading cards", () => {
+    describe("initialise a game with two players", () => {
+        let game;
+        beforeEach(() => {
+            game = new Game();
+        });
+
+        it("each with 30 health", () => {
+            expect(game.player1.health).toBe(30)
+            expect(game.player2.health).toBe(30)
+        });
+
+        it("each with 0 mana slots", () => {
+            expect(game.player1.manaSlot).toBe(0)
+            expect(game.player2.manaSlot).toBe(0)
+        });
+
+        it("each with 0 active mana", () => {
+            expect(game.player1.activeMana).toBe(0)
+            expect(game.player2.activeMana).toBe(0)
+        });
+
+        it("each having the expected 20 mana cards", () => {
+            const expectedCards = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]
+            expect([...game.player1.cards, ...game.player1.cardsInHand].sort()).toEqual(expectedCards)
+            expect([...game.player2.cards, ...game.player2.cardsInHand].sort()).toEqual(expectedCards)
+        });
+
+        it("each having 3 cards in hand", () => {
+            expect(game.player1.cardsInHand.length).toBe(3)
+            expect(game.player2.cardsInHand.length).toBe(3)
+        });
+    });
+
+    describe("on game start", () => {
+        let game;
+        beforeEach(() => {
+            game = new Game();
+        });
+
+        it("the active player's mana slot has increased by 1", () => {
+            game.turnStart();
+            expect(game.player1.manaSlot).toBe(1)
+        });
+
+        it("the active player's active mana is refilled", () => {
+            game.turnStart();
+            expect(game.player1.activeMana).toBe(1)
+        });
+
+        it("the active player's receives 1 more card", () => {
+            game.turnStart();
+            expect(game.player1.cardsInHand.length).toBe(4)
+        });
+
+        it("the active player's deck size is reduced by 1", () => {
+            const expectedDeckSize = game.player1.cards.length - 1
+            game.turnStart();
+            expect(game.player1.cards.length).toBe(expectedDeckSize)
+        });
+    });
+});
